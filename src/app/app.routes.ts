9def880d23afce61b4e5259b3fc03bbfb5b759e5
jguillevic@isagri.fr <jguillevic-isagri>
2023-12-19 17:20:29 +0100

    Création side project Eco+

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/page/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        title: 'Acceuil',
+        path: 'home',
+        component: HomePageComponent 
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
