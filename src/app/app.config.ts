ec8f09e02c74db5392f7bf815b6a0ec1c87b1f96
jguillevic@isagri.fr <jguillevic-isagri>
2023-12-21 16:54:34 +0100

    Suppression BrowserAnimationModule car inutile.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes, withViewTransitions()),
         provideAnimations(),
-        BrowserAnimationsModule
     ]
 };
